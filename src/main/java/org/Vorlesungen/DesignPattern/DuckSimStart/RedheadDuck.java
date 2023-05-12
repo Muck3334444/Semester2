d6f6558dc0d9aa24adc2a4b54cdf790393239706
653222012 <larichgo@web.de>
2023-05-12 08:17:23 +0200

    Enten

---
+++ src/main/java/org/Vorlesungen/DesignPattern/DuckSimStart/RedheadDuck.java
@@ -0,0 +1,11 @@
+package org.Vorlesungen.DesignPattern.DuckSimStart;
+
+public class RedheadDuck extends Duck {
+
+	@Override
+	public void display() {
+		System.out.println("I am a Redhead duck.");
+
+	}
+
+}
